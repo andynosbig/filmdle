2087018d69270912aa86d95b8547dfee20e0b0dc
andynosbig <andynosbig@hotmail.com>
2022-02-19 22:51:07 +0000

    change principals db

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # moviele
-load_imdb.py combined the source imdb datasets into 2 files, combined.tsv which is a filtered table of films and principal ids, name.short.tsv which is a principals id to name table.
+load_imdb.py combined the source imdb datasets into 2 files, combined.tsv which is a filtered table of films and principal.tsv
+Uses imdb data as described here: https://www.imdb.com/interfaces/
 moveiele.ipynb is an in progress implementation of moviele
--- load_imdb.py
+++ load_imdb.py
@@ -36,10 +36,6 @@ with open("title.basics.tsv", 'r') as basics:
         film["genres"] = row['genres']
         film.setdefault('principals', [])
         films[film["id"]]= film
-        
-        # test_count += 1
-        # if test_count == 1000:
-        #     break
 
 rated_films = {}
 with open("title.ratings.tsv", 'r') as ratings:
@@ -52,26 +48,28 @@ with open("title.ratings.tsv", 'r') as ratings:
 
 print(len(rated_films))
 films = rated_films
-# with open("title.principals.tsv", 'r') as principals:
-#     reader = csv.DictReader(principals, delimiter='\t')
-#     for row in reader:
-#         filmId = row['tconst']
-#         if filmId in films:
-#             films[filmId].setdefault('principals', []).append(row['nconst'])
+all_principals = set()
+with open("title.principals.tsv", 'r') as principals:
+    reader = csv.DictReader(principals, delimiter='\t')
+    for row in reader:
+        filmId = row['tconst']
+        if filmId in films and row['category'] in ["actor", "actress", "director"]:
+            films[filmId].setdefault('principals', []).append(row['nconst'])
+            all_principals.add(row['nconst'])
 
-important_principals = {}
+principal_names = {}
 with open("name.basics.tsv", 'r') as names:
     reader = csv.DictReader(names, delimiter='\t')
     for row in reader:
-        knownFors = row['knownForTitles'].split(',')
-        if "actor" not in row['primaryProfession'] and "director" not in row['primaryProfession']:
-            continue
-        if len(knownFors) < 2:
-            continue
-        for knownFor in knownFors:
-            if knownFor in films:
-                films[knownFor].setdefault('principals', []).append(row['primaryName'])
-                break
+        if row['nconst'] in all_principals:
+            principal_names[row['nconst']] = row['primaryName']
+
+with open('principals.tsv', 'w', newline='') as tsvfile:
+    writer = csv.DictWriter(tsvfile, fieldnames=["id", "name"], delimiter='\t')
+
+    writer.writeheader()
+    for id in principal_names:
+        writer.writerow({"id": id, "name": principal_names[id]})
 
 with open('combined.tsv', 'w', newline='') as tsvfile:
     fieldnames = list(films.values())[0].keys()
